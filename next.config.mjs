839b0a7a87ee54c3d270d3838d8e23830de419a6
vipul-bhatia <vipul0592bhatia@gmail.com>
2024-02-14 00:52:11 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+      },
+      eslint: {
+         ignoreDuringBuilds: true,
+     },
+     typescript: {
+         ignoreBuildErrors: true
+     }
+};
 
 export default nextConfig;
