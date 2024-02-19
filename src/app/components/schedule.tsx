b085f3e0baebe81bbbcdd29fda23835a0d509037
vipul-bhatia <vipul0592bhatia@gmail.com>
2024-02-19 10:05:36 +0530

    .

--- src/app/components/schedule.tsx
+++ src/app/components/schedule.tsx
@@ -5,7 +5,7 @@ interface Match {
     date: string;
     team1: string;
     team2: string;
-   timing : string;
+    timing: string;
 }
 
 const Schedule = ({ sport }: { sport: string }) => {
@@ -18,29 +18,33 @@ const Schedule = ({ sport }: { sport: string }) => {
             .catch((error) => console.error("Fetching error:", error));
     }, [sport]);
 
-  return (
-    <div className="overflow-auto" style={{ maxHeight: '500px' }}>
-      <h2 className="text-2xl font-semibold text-gray-800 mb-4">Today's Schedule for {sport}</h2>
-      <table className="min-w-full table-auto">
-        <thead className="bg-gray-200">
-          <tr> 
-            <th className="px-4 py-2 text-left text-gray-600">Date</th>
-            <th className="px-4 py-2 text-left text-gray-600">Match</th>
-            <th className="px-4 py-2 text-left text-gray-600">Timing</th>
-          </tr>
-        </thead>
-        <tbody>
-          {matches.map((match, index) => (
-            <tr className={`hover:bg-gray-100 ${index % 2 === 0 ? 'bg-white' : 'bg-gray-50'}`}>
-              <td className="border px-4 py-2 text-gray-700">{match.date}</td>
-              <td className="border px-4 py-2 text-gray-700">{match.team1} vs {match.team2}</td>
-              <td className="border px-4 py-2 text-gray-700">{match.timing}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    return (
+        <div className="overflow-auto" style={{ maxHeight: '500px' }}>
+            <h2 className="text-2xl font-semibold text-gray-800 mb-4">Today's Schedule for {sport}</h2>
+            {matches.length > 0 ? (
+                <table className="min-w-full table-auto">
+                    <thead className="bg-gray-200">
+                        <tr>
+                            <th className="px-4 py-2 text-left text-gray-600">Date</th>
+                            <th className="px-4 py-2 text-left text-gray-600">Match</th>
+                            <th className="px-4 py-2 text-left text-gray-600">Timing</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {matches.map((match, index) => (
+                            <tr className={`hover:bg-gray-100 ${index % 2 === 0 ? 'bg-white' : 'bg-gray-50'}`} key={index}>
+                                <td className="border px-4 py-2 text-gray-700">{match.date}</td>
+                                <td className="border px-4 py-2 text-gray-700">{match.team1} vs {match.team2}</td>
+                                <td className="border px-4 py-2 text-gray-700">{match.timing}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            ) : (
+                <div className="text-center text-gray-800">No matches today</div>
+            )}
+        </div>
+    );
 };
 
 export default Schedule;
--- src/app/components/standings.tsx
+++ src/app/components/standings.tsx
@@ -1,47 +1,50 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface StandingsProps {
-  id: number;
-  date: string;
-  team1: string;
-  team2: string;
- Winner : string;
+    id: number;
+    date: string;
+    team1: string;
+    team2: string;
+    Winner: string;
 }
 
 const Standings = ({ sport }: { sport: string }) => {
-  const [matches, setMatches] = useState<StandingsProps[]>([]);
+    const [matches, setMatches] = useState<StandingsProps[]>([]);
 
-  useEffect(() => {
-      fetch(`/data/${sport}.json`)
-          .then((response) => response.json())
-          .then((data) => setMatches(data.standings))
-          .catch((error) => console.error("Fetching error:", error));
-  }, [sport]);
+    useEffect(() => {
+        fetch(`/data/${sport}.json`)
+            .then((response) => response.json())
+            .then((data) => setMatches(data.standings))
+            .catch((error) => console.error("Fetching error:", error));
+    }, [sport]);
 
-return (
-  <div className="overflow-x-auto">
-    <h2 className="text-2xl font-semibold text-gray-800 mb-4">Today's Standings for {sport}</h2>
-    <table className="min-w-full table-auto">
-      <thead className="bg-gray-200">
-        <tr> 
-          <th className="px-4 py-2 text-left text-gray-600">Date</th>
-          <th className="px-4 py-2 text-left text-gray-600">Match</th>
-          <th className="px-4 py-2 text-left text-gray-600">Winner</th>
-        </tr>
-      </thead>
-      <tbody>
-        {matches.map((match, index) => (
-          <tr className={`hover:bg-gray-100 ${index % 2 === 0 ? 'bg-white' : 'bg-gray-50'}`}>
-            <td className="border px-4 py-2 text-gray-700">{match.date}</td>
-            <td className="border px-4 py-2 text-gray-700">{match.team1} vs {match.team2}</td>
-            <td className="border px-4 py-2 text-gray-700">{match.Winner}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
+    return (
+        <div className="overflow-auto" style={{ maxHeight: '500px' }}>
+            <h2 className="text-2xl font-semibold text-gray-800 mb-4">Today's Standings for {sport}</h2>
+            {matches.length > 0 ? (
+                <table className="min-w-full table-auto">
+                    <thead className="bg-gray-200">
+                        <tr>
+                            <th className="px-4 py-2 text-left text-gray-600">Date</th>
+                            <th className="px-4 py-2 text-left text-gray-600">Match</th>
+                            <th className="px-4 py-2 text-left text-gray-600">Winner</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {matches.map((match, index) => (
+                            <tr className={`hover:bg-gray-100 ${index % 2 === 0 ? 'bg-white' : 'bg-gray-50'}`} key={index}>
+                                <td className="border px-4 py-2 text-gray-700">{match.date}</td>
+                                <td className="border px-4 py-2 text-gray-700">{match.team1} vs {match.team2}</td>
+                                <td className="border px-4 py-2 text-gray-700">{match.Winner}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            ) : (
+                <div className="text-center text-gray-800">No matches today</div>
+            )}
+        </div>
+    );
 };
 
 export default Standings;
-
